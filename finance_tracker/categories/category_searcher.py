4ec166b6f1494f23eeaad5de0edf1bbab218b666
Ramon <ramon.guimera@skyscanner.net>
2022-10-07 11:06:48 +0200

    change categories to be methods

--- finance_tracker/categories/category_searcher.py
+++ finance_tracker/categories/category_searcher.py
@@ -1,13 +1,13 @@
 from functools import cache
 
-from finance_tracker.categories.categories import CATEGORIES_ITEMS, DEFAULT_CATEGORY
+from finance_tracker.categories.categories import categories_items, DEFAULT_CATEGORY
 from finance_tracker.printer import bcolors
 
 
 class CategorySearcher:
     @cache
     def search_category(self, title: str) -> str:
-        for category, categorized_items in CATEGORIES_ITEMS.items():
+        for category, categorized_items in categories_items().items():
             if title in categorized_items:
                 return category
         bcolors.print_warning(f"WARNING - No category detected for title <{title}>, proceeding with default")
