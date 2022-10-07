286efd202775e3c37834f72442d122d2870948e2
Ramon <ramon.guimera@skyscanner.net>
2022-10-07 11:06:55 +0200

    mock methods

--- tests/categories/test_category_searcher.py
+++ tests/categories/test_category_searcher.py
@@ -1,15 +1,21 @@
 import time
+from unittest.mock import patch, MagicMock
 
 from finance_tracker.categories.category_searcher import CategorySearcher
 
 
-def test_category_searcher_returns_na_if_category_not_found():
+@patch("finance_tracker.categories.categories.all_categories")
+def test_category_searcher_returns_na_if_category_not_found(patched_all_categories: MagicMock):
+    patched_all_categories.return_value = {'CATEGORIES': {'PAYCHECK': ['PAYCHECK_FROM_COMPANY']}, 'POSITIVE_CATEGORIES': []}
     searcher = CategorySearcher()
     result = searcher.search_category(title="random")
     assert result == "n/a"
 
 
-def test_category_searcher_returns_a_category_if_category_category_for_title_exists():
+@patch("finance_tracker.categories.categories.all_categories")
+def test_category_searcher_returns_a_category_if_category_category_for_title_exists(patched_all_categories: MagicMock):
+    patched_all_categories.return_value = {'CATEGORIES': {'PAYCHECK': ['PAYCHECK_FROM_COMPANY']},
+                                           'POSITIVE_CATEGORIES': []}
     searcher = CategorySearcher()
     result = searcher.search_category(title="PAYCHECK_FROM_COMPANY")
     assert result == "PAYCHECK"
