9370bee752f1e0961407a06937df7a152dc37ccb
Ramon <ramon.guimera@skyscanner.net>
2022-10-18 17:35:17 +0200

    improving tests

--- tests/categories/test_category_searcher.py
+++ tests/categories/test_category_searcher.py
@@ -24,18 +24,14 @@ def test_category_searcher_returns_a_category_if_category_category_for_title_exi
 
 @patch("finance_tracker.categories.categories.all_categories")
 def test_category_searcher_cache_works(patched_all_categories: MagicMock, all_categories):
-    """
-    If the cache of the searcher gets deleted, this test still passes :shrug:
-    It may need a better way to check it works.
-    todo - the better way is to test this,
-    by searching for a category, let's say, 500 times and see that the print/logger
-    only outputs once that it hasn't found a category
-    """
     patched_all_categories.return_value = all_categories
     searcher = CategorySearcher()
     first_time_start = time.time()
-    searcher.search_category(title="NOMINA")
+    searcher.search_category(title="PAYCHECK_FROM_COMPANY")
     first_time_end = time.time()
-    searcher.search_category(title="NOMINA")
-    second_time_end = time.time()
-    assert (second_time_end - first_time_end) <= (first_time_end - first_time_start)
+
+    for _ in range(500):
+        second_time_start = time.time()
+        searcher.search_category(title="PAYCHECK_FROM_COMPANY")
+        second_time_end = time.time()
+        assert (second_time_end - second_time_start) <= (first_time_end - first_time_start)
