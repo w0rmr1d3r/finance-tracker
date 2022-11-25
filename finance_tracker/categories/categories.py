051e52bf6d73cf967e992fafcb48c58fdfe36b58
Ramon <ramon.guimera@skyscanner.net>
2022-11-25 17:01:21 +0100

    fix - return default categories if file doesn't exist

--- finance_tracker/categories/categories.py
+++ finance_tracker/categories/categories.py
@@ -3,17 +3,22 @@ import pathlib
 from typing import Set
 
 DEFAULT_CATEGORY = "n/a"
+DEFAULT_CATEGORIES = {"CATEGORIES": {}, "POSITIVE_CATEGORIES": []}
 
 
 def all_categories():
     """
     Retrieves all the categories from the "categories.json" file.
+    If the file weren't found, it will return <DEFAULT_CATEGORIES>
 
     :return: Python dict retrieved from the categories file
     """
     current_path = pathlib.Path(__file__).parent.resolve()
-    with open(f"{current_path}/../../load/categories/categories.json", "r", encoding="ASCII") as file:
-        return json.load(file)
+    try:
+        with open(f"{current_path}/../../load/categories/categories.json", "r", encoding="ASCII") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return DEFAULT_CATEGORIES
 
 
 def categories_items():
