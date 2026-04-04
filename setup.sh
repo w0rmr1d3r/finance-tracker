90721a2da8c5113fc7eef0adef1cd2b80633cbad
Ramon <ramon.guimera@skyscanner.net>
2026-04-04 16:02:42 +0200

    PR comments

--- setup.sh
+++ setup.sh
@@ -6,12 +6,14 @@ mkdir -p backend/load/entries_files/revolut
 mkdir -p backend/load/entries_files/santander
 mkdir -p backend/saved_files
 
-cat > backend/load/categories/categories.json << 'EOF'
+if [ ! -f backend/load/categories/categories.json ]; then
+    cat > backend/load/categories/categories.json << 'EOF'
 {
     "CATEGORIES": {
     },
     "POSITIVE_CATEGORIES": []
 }
 EOF
+fi
 
 echo "Setup complete."
