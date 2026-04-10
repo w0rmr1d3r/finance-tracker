e6070fa4cca5f2ec14ff0e29780e39595c7cc359
Ramon <ramon.guimera@skyscanner.net>
2026-04-10 17:59:19 +0200

    improve logging

--- backend/entrypoint.py
+++ backend/entrypoint.py
@@ -1,3 +1,17 @@
+import copy
+import logging
+
 import uvicorn
 
-uvicorn.run("finance_tracker.__main__:app", host="0.0.0.0", port=8000)
+log_config = copy.deepcopy(uvicorn.config.LOGGING_CONFIG)
+log_config["formatters"]["default"]["fmt"] = "%(asctime)s - %(levelname)s - %(message)s"
+log_config["formatters"]["default"]["datefmt"] = "%Y-%m-%d %H:%M:%S"
+log_config["loggers"]["finance_tracker"] = {"handlers": ["default"], "level": "INFO", "propagate": False}
+
+uvicorn.run(
+    "finance_tracker.__main__:app",
+    host="0.0.0.0",
+    port=8000,
+    log_level=logging.getLevelName(logging.INFO).lower(),
+    log_config=log_config,
+)
