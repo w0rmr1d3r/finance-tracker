f3d5b074d0ea10459a6ab07c0bbc17760c00dc30
Ramon <ramon.guimera@skyscanner.net>
2026-04-15 19:50:59 +0200

    pr comments

--- backend/entrypoint.py
+++ backend/entrypoint.py
@@ -1,5 +1,4 @@
 import copy
-import logging
 
 import uvicorn
 
@@ -12,6 +11,6 @@ uvicorn.run(
     "finance_tracker.__main__:app",
     host="0.0.0.0",
     port=8000,
-    log_level=logging.getLevelName(logging.INFO).lower(),
+    log_level="info",
     log_config=log_config,
 )
